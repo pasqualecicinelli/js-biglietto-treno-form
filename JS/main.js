7a3a77bd15b15100f2cdf22fe0aa7e8dbb92925d
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2023-07-20 18:16:15 +0200

    feat style

--- JS/main.js
+++ JS/main.js
@@ -6,7 +6,9 @@ const age = document.getElementById('age-input');
 
 const buttonSend = document.getElementById('send');
 
-const printInput = document.getElementById('print');
+const printName = document.getElementById('print-name');
+const printDiscount = document.getElementById('print-discount');
+const printPrice = document.getElementById('print-price');
 
 
 buttonSend.addEventListener('click', function () {
@@ -16,8 +18,6 @@ buttonSend.addEventListener('click', function () {
     console.log(km.value);
     console.log(age.value);
     console.log(inputName);
-
-    printInput.innerHTML = inputName;
 
     const price = (km.value * 0.21);
     console.log(price);
@@ -30,22 +30,33 @@ buttonSend.addEventListener('click', function () {
         const priceMin = (price - discount);
         console.log(priceMin);
 
-        
-        printInput.innerHTML = priceMin.toFixed(2);
-        
+        printName.innerHTML = nameSurname.value;
+        printDiscount.innerHTML = discount.toFixed(2) + '€' + ' E\' stato applicato il 20% di sconto';
+        printPrice.innerHTML = priceMin.toFixed(2) + '€';
+
     } else if (age.value == 'over') {
 
         const discount = (price * 0.4);
         console.log(discount);
         const priceOver = (price - discount);
         console.log(priceOver);
-        printInput.innerHTML = priceOver.toFixed(2);
+
+        printName.innerHTML = nameSurname.value;
+        printDiscount.innerHTML = discount.toFixed(2) + '€' + ' E\' stato applicato il 40% di sconto';
+        printPrice.innerHTML = priceOver.toFixed(2) + '€';
 
     }
 
     else {
 
         console.log(price);
-        printInput.innerHTML = price.toFixed(2);
+        printName.innerHTML = nameSurname.value;
+        printDiscount.innerHTML = 'Biglietto base';
+        printPrice.innerHTML = price.toFixed(2) + '€';
     }
+
+    nameSurname.value = '';
+    km.value = '';
+    age.value = '';
+
 })
